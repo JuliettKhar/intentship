e3fac818e0238f29704673a4df7bf9e96b391366
Julia Kharlamova <yuliya.kharlamova@rutorika.ru>
2019-04-06 20:14:37 +0300

    update styles

--- script.js
+++ script.js
@@ -7,43 +7,47 @@ function findElements() {
 	worksLinks = document.querySelector('.worksLinks');
 	worksLink = [].slice.call(document.querySelectorAll('.worksLink'));
 	worksDescription = [].slice.call(document.querySelectorAll('.worksDescription'));
-	console.log(worksLinks, worksLink, worksDescription);
 	activeTab = 0;
 }
 
 function switchTabsContent(index) {
+	if ( index !== 0) worksDescription.forEach( item => item.classList.remove('tabsContent-is-active')); 
 	worksDescription[activeTab].classList.remove('tabsContent-is-active');
-	index = activeTab;
+	activeTab = index;
 	worksDescription[activeTab].classList.add('tabsContent-is-active');
+
 }
 
-function isInactiveTab(index) {
+function deactivateTab(index) {
 	return index !== activeTab;
 }
 
 
 function switchTabs(index){
-	console.log(index, 'index, switchTabs')
-	if(isInactiveTab(index)) switchTabsContent(index);
+	if(deactivateTab(index)) switchTabsContent(index);
+	
+}
+
+function showAll(index) {
+	 worksDescription.forEach( item => item.classList.add('tabsContent-is-active'));
 }
 
 function onClick(event) {
 	event.preventDefault();
 	const { target } = event;
-	console.log(target, 'target,onClick');
 	const index = worksLink.indexOf(target);
-	console.log(index, 'target,onClick');
-	if (index >= 0) switchTabs(index, target);
-	console.log('!!!', 'target,onClick');
+	if (index > 0) switchTabs(index, target);
+	if (index === 0) showAll(index);
+
 }
 
 function subscribe() {
 	worksLinks.addEventListener('click', onClick);
-
 }
 
 function init() {
 	findElements();
 	subscribe();
  }
+
 window.onload = init;
